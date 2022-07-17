ccbf1e8d4ff29477ead145164d8a4948ebaef0bf
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-07-17 17:04:27 +0300

    homework - dictionary changed task 1

--- tasks.py
+++ tasks.py
@@ -5,6 +5,9 @@ dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 merge_dict = dictionary_1.copy()
 merge_dict.update(dictionary_2)
+# merge_dict = {**dictionary_1, **dictionary_2}
+# merge_dict = dictionary_1 | dictionary_2
+# dictionary_1.update(dictionary_2)  # print(dictionary_1)
 print(merge_dict)
 
 
