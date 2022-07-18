6af896d4bae1e7ee4b1b0e3790b5c58ea0edf87e
Turchin_Alexander <ymeleca@gmail.com>
2022-07-18 09:20:25 +0300

    changed task 4

--- tasks.py
+++ tasks.py
@@ -47,3 +47,28 @@ list_1 = [1, 2, 3]
 list_2 = [10, 20, 30]
 dictionary = dict(zip(list_1, list_2))
 print(dictionary)
+
+print()
+
+dictionary = {}
+for i in range(len(list_1)):
+    dictionary[list_1[i]] = list_2[i]
+print(dictionary)
+
+print()
+
+x = 0
+dictionary = {}
+for i in list_1:
+  dictionary[i] = dictionary.get(i, 0) + list_2[x]
+  x += 1
+print(dictionary)
+
+print()
+
+x = 0
+dictionary = {}
+for i in list_1:
+  dictionary[i] = list_2[x]
+  x += 1
+print(dictionary)
